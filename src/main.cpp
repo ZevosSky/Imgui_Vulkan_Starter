144a11f30439aa419c880fd4ae0b51aff89c5049
ZevosSky <120239831+ZevosSky@users.noreply.github.com>
2025-06-18 16:08:52 -0700

    base file structure

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/// @author Gary Yang
+/// @date 6/18/2025
+/// @copyright Copyright (c) 2025, Gary Yang
+/// @license This project is released under the MIT License.
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+    std::cout << "Application started successfully!" << std::endl;
+
+    // Main application logic goes here
+    // ...
+
+
+
+    return 0; // Return success code
+}
